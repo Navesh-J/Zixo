a587b55aafb420499f2fb6cb74e1f58c38f1c6ff
Navesh Jaiswal <navesh.professional@gmail.com>
2026-01-31 02:20:34 +0530

    Initial setup for central server

---
+++ discovery-server/src/test/java/com/zixo/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zixo.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
