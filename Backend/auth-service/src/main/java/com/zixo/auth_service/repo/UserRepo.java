6e0cd9452961b601829cfda2c1e2b7669a312ef9
Navesh Jaiswal <navesh.professional@gmail.com>
2026-02-03 14:40:40 +0530

    Repository layer

---
+++ Backend/auth-service/src/main/java/com/zixo/auth_service/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.zixo.auth_service.repo;
+
+import com.zixo.auth_service.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+    boolean existsByUsername(String username);
+}
