a9bb7ea8a65438eaf18bda53a1776202090bd45b
Navesh Jaiswal <navesh.professional@gmail.com>
2026-02-03 14:38:18 +0530

    data models

---
+++ Backend/auth-service/src/main/java/com/zixo/auth_service/model/Role.java
@@ -0,0 +1,6 @@
+package com.zixo.auth_service.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- Backend/auth-service/src/main/java/com/zixo/auth_service/model/User.java
+++ Backend/auth-service/src/main/java/com/zixo/auth_service/model/User.java
@@ -1,4 +1,27 @@
 package com.zixo.auth_service.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = "username")})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
 }
