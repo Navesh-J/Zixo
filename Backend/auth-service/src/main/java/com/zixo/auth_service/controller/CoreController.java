4e31cb23da209eee9a0a44a14996dbd87e2121da
Navesh Jaiswal <navesh.professional@gmail.com>
2026-02-03 22:22:58 +0530

    Core controller

---
+++ Backend/auth-service/src/main/java/com/zixo/auth_service/controller/CoreController.java
@@ -0,0 +1,17 @@
+package com.zixo.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoreController {
+    @GetMapping("/ping")
+    public String ping() {
+        return "Backend Pinged: Starting the Server....";
+    }
+
+    @GetMapping("/health")
+    public String health() {
+        return "Auth Service is UP";
+    }
+}
