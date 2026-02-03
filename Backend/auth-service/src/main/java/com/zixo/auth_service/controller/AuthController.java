c93b7b73f4eb399f0b3c0b1442234ab1811361ce
Navesh Jaiswal <navesh.professional@gmail.com>
2026-02-03 14:39:05 +0530

    Rest Controller

---
+++ Backend/auth-service/src/main/java/com/zixo/auth_service/controller/AuthController.java
@@ -0,0 +1,37 @@
+package com.zixo.auth_service.controller;
+
+import com.zixo.auth_service.dto.AuthRequest;
+import com.zixo.auth_service.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@Valid @RequestBody AuthRequest request) {
+
+        authService.register(request.getUsername(), request.getPassword());
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("User registered successfully");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@Valid @RequestBody AuthRequest request) {
+
+        String token = authService.login(request.getUsername(), request.getPassword());
+
+        return ResponseEntity.ok(token);
+    }
+}
